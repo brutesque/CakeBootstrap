dbb5c75078f323710e6dc732aa29334cf5741dca
Alain <alain@brutesque.nl>
2014-05-14 04:08:09 +0200

    custom ui improvements

--- webroot/js/cake-bootstrap.js
+++ webroot/js/cake-bootstrap.js
@@ -1,10 +1,27 @@
-$('textarea').autosize();
-$(".select2").select2();
-$(".datepicker").datepicker({
-	format: "yyyy-mm-dd", 
-    todayBtn: "linked", 
-    calendarWeeks: true, 
-    autoclose: false, 
-    todayHighlight: true, 
-    startView: 1
+$(function(){
+
+	uiTools();
+
 });
+
+$( document ).ajaxComplete(function(event, xhr, settings) {
+
+	uiTools();
+
+});
+
+function uiTools() {
+
+	$('textarea').autosize();
+	$(".select2").select2();
+	$(".datepicker").datepicker({
+		format: "yyyy-mm-dd", 
+	    todayBtn: "linked", 
+	    calendarWeeks: true, 
+	    autoclose: false, 
+	    todayHighlight: true, 
+	    startView: 1
+	});
+
+}
+
